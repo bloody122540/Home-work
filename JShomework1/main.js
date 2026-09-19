a1378b2b70389425f625491563bf746bfe6b22f8
bloody122540 <bloody122540@gmail.com>
2026-09-19 15:42:52 +0300

    Add files via upload

---
+++ JShomework1/main.js
@@ -0,0 +1,34 @@
+const firstName = "Антон";
+const lastName = " Власов";
+let str = firstName + lastName;
+console.log(str);
+
+const isStudent = true;
+const isNotStudent = false;
+console.log(typeof false);
+
+
+let age = 31;
+let currentYear = 2026;
+let sum = currentYear - age;
+console.log(sum)
+
+
+let proposal = "Меня зовут " + firstName + lastName + " мне " + age + " год." + " Я ученик курса:" + isStudent;
+console.log(proposal);
+
+
+let a = `123`;
+let b = +`456`;
+let c = Number(`789`);
+let d = Boolean(0);
+let e = Boolean(``);
+let result = a + b + c + d + e;
+console.log(result);
+
+// 123 + 12450 
+
+
+
+
+
